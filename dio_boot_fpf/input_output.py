8d88fed0b1a1549d0d036f30ee8597bcdf9b9cf9
Cataldinata <natacataldi@gmail.com>
2024-10-27 21:53:58 -0300

    function input()

---
+++ dio_boot_fpf/input_output.py
@@ -0,0 +1,10 @@
+#receber(input) e exibir(print) informações para nossos usuários
+
+nome = input("Informe o seu nome: ")
+idade = input("Informe sua idade: ")
+
+
+print(nome, idade)
+print(nome, idade, end=" ")
+print(nome, idade, end="... \n")
+print(nome, idade, sep="#")
